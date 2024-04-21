1c505ef3eedee3d4f4cf98097b5476edf5d06899
NJashua <nithinjashua1@gmail.com>
2024-04-22 00:02:45 +0530

    its use to know about season based on month and day

---
+++ IfElsestatements/primenumber.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+is_prime = True
+
+if num <= 1:
+    is_prime = False
+else:
+    for i in range(2, int(num * 0.5) + 1):
+        if num % 2 == 0:
+            is_prime = False
+            break
+if is_prime:
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
