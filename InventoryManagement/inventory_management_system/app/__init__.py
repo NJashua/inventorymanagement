d3bb53e20be6a874bdd0d3bb05c14c182da8a0f2
NJashua <nithinjashua1@gmail.com>
2024-05-22 12:11:15 +0530

    here with a new project called inventory management, enjoy

--- InventoryManagement/inventory_management_system/app/__init__.py
+++ InventoryManagement/inventory_management_system/app/__init__.py
@@ -1,11 +1,7 @@
-# app/__init__.py
-
 from flask import Flask
-from config import Config
+from app.routes import bp
 
-# Initialize Flask app
-app = Flask(__name__)
-app.config.from_object(Config)
-
-# Import routes
-from app import routes
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(bp)
+    return app
