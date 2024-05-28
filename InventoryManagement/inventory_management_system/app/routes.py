41dd3cdd5ca589fbc940da5b669f34e80a1e0fba
NJashua <nithinjashua1@gmail.com>
2024-05-28 18:44:22 +0530

    final code..:)

--- InventoryManagement/inventory_management_system/app/routes.py
+++ InventoryManagement/inventory_management_system/app/routes.py
@@ -1,4 +1,41 @@
-from flask import Blueprint, request
+# from flask import Blueprint, request, jsonify
+# from app.db import SnowflakeDB
+
+# # Create Blueprint for the routes
+# bp = Blueprint('routes', __name__)
+
+# # Configuration for Snowflake
+# snowflake_config = {
+#     'user': 'Nithin',
+#     'password': 'Nithin@2024',
+#     'account': 'bdhriyc-ke24872',
+#     'database': 'INVENTORY',
+#     'schema': 'PUBLIC'
+# }
+
+# # Initialize SnowflakeDB instance
+# db = SnowflakeDB(snowflake_config)
+
+# @bp.route('/search=<search_term>', methods=['GET'])
+# def search_products(search_term):
+#     return db.get_product_details_with_location(search_term)
+
+# @bp.route('/display_products', methods=['GET'])
+# def display_products():
+#     return db.display_products_details()
+
+# @bp.route('/purchase_product', methods=['POST'])
+# def insert():
+#     data = request.get_json()
+#     return db.insert_data(data)
+
+# @bp.route('/order_product', methods=['POST'])
+# def order_product():
+#     data = request.get_json()
+#     return db.order_service_details(data)
+
+
+from flask import Blueprint, request, jsonify
 from app.db import SnowflakeDB
 
 # Create Blueprint for the routes
@@ -16,8 +53,9 @@ snowflake_config = {
 # Initialize SnowflakeDB instance
 db = SnowflakeDB(snowflake_config)
 
-@bp.route('/search/<search_term>', methods=['GET'])
-def search_products(search_term):
+@bp.route('/search', methods=['GET'])
+def search_products():
+    search_term = request.args.get('search_term')
     return db.get_product_details_with_location(search_term)
 
 @bp.route('/display_products', methods=['GET'])
@@ -28,11 +66,8 @@ def display_products():
 def insert():
     data = request.get_json()
     return db.insert_data(data)
-@bp.route('/order_product', methods = ['POST', 'GET'])
-def order_product():
-   data = request.get_json()
-   return db.order_service_details(data)
 
-# @bp.route('/search_with_location/<search_term>', methods=['GET'])
-# def search_product_with_location(search_term):
-#     return db.search_product_with_location(search_term)
+@bp.route('/order_product', methods=['POST'])
+def order_product():
+    data = request.get_json()
+    return db.order_service_details(data)
--- InventoryManagement/inventory_management_system/config.py
+++ InventoryManagement/inventory_management_system/config.py
@@ -1,7 +1,27 @@
 
+# class Config:
+#     SNOWFLAKE_USER = 'Nithin',
+#     SNOWFLAKE_PASSWORD = 'Nithin@2024',
+#     SNOWFLAKE_ACCOUNT = 'bdhriyc-ke24872',
+#     SNOWFLAKE_DATABASE = 'INVENTORY',
+#     SNOWFLAKE_SCHEMA = 'PUBLIC'
+
+
+# inventory_management_system/config.py
+
 class Config:
-    SNOWFLAKE_USER = 'Nithin',
-    SNOWFLAKE_PASSWORD = 'Nithin@2024',
-    SNOWFLAKE_ACCOUNT = 'bdhriyc-ke24872',
-    SNOWFLAKE_DATABASE = 'INVENTORY',
+    SNOWFLAKE_USER = 'Nithin'
+    SNOWFLAKE_PASSWORD = 'Nithin@2024'
+    SNOWFLAKE_ACCOUNT = 'bdhriyc-ke24872'
+    SNOWFLAKE_DATABASE = 'INVENTORY'
     SNOWFLAKE_SCHEMA = 'PUBLIC'
+
+    @staticmethod
+    def as_dict():
+        return {
+            'user': Config.SNOWFLAKE_USER,
+            'password': Config.SNOWFLAKE_PASSWORD,
+            'account': Config.SNOWFLAKE_ACCOUNT,
+            'database': Config.SNOWFLAKE_DATABASE,
+            'schema': Config.SNOWFLAKE_SCHEMA
+        }
