bb60a1a8e311626512153e268bd79b06b35aba4b
NJashua <nithinjashua1@gmail.com>
2024-05-29 15:30:46 +0530

    final code

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import inventory_management
+from flask import jsonify
+from app import db
 
 app = inventory_management()
+@app.route('/', methods=['GET'])
+def check_status():
+    return jsonify({"status": "ok", "message": "API is running"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
