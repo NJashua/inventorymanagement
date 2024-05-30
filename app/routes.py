a46b4150486feadb0429dfc750c93bb505f12633
Nithin Burugu <139949160+NJashua@users.noreply.github.com>
2024-05-30 23:24:50 +0530

    improve

--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,6 @@
-# from flask import Blueprint, request, jsonify
-# from app.db import SnowflakeDB
-
-# # Create Blueprint for the routes
-# bp = Blueprint('routes', __name__)
-
-# # Configuration for Snowflake
-# snowflake_config = {
-#     'user': 'Nithin',
-#     'password': 'Nithin@2024',
-#     'account': 'bdhriyc-ke24872',
-#     'database': 'INVENTORY',
-#     'schema': 'PUBLIC'
-# }
-
-# # Initialize SnowflakeDB instance
-# db = SnowflakeDB(snowflake_config)
-
-# @bp.route('/search=<search_term>', methods=['GET'])
-# def search_products(search_term):
-#     return db.get_product_details_with_location(search_term)
-
-# @bp.route('/display_products', methods=['GET'])
-# def display_products():
-#     return db.display_products_details()
-
-# @bp.route('/purchase_product', methods=['POST'])
-# def insert():
-#     data = request.get_json()
-#     return db.insert_data(data)
-
-# @bp.route('/order_product', methods=['POST'])
-# def order_product():
-#     data = request.get_json()
-#     return db.order_service_details(data)
-
-
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response
 from app.db import SnowflakeDB
+from app.utils import error_response
 
 # Creating Blueprint for the routes
 bp = Blueprint('routes', __name__)
@@ -44,30 +8,4 @@ bp = Blueprint('routes', __name__)
 # Configuration for Snowflake
 snowflake_config = {
     'user': 'Nithin',
-    'password': 'Nithin@2024',
-    'account': 'bdhriyc-ke24872',
-    'database': 'INVENTORY',
-    'schema': 'PUBLIC'
-}
-
-# Initialize SnowflakeDB instance
-db = SnowflakeDB(snowflake_config)
-
-@bp.route('/search', methods=['GET'])
-def search_products():
-    search_term = request.args.get('search_term')
-    return db.get_product_details_with_location(search_term)
-
-@bp.route('/display_products', methods=['GET'])
-def display_products():
-    return db.display_products_details()
-
-@bp.route('/purchase_product', methods=['POST'])
-def insert():
-    data = request.get_json()
-    return db.insert_purchase_data(data)
-
-@bp.route('/order_product', methods=['POST'])
-def order_product():
-    data = request.get_json()
-    return db.order_service_details(data)
+    'password': 'Nithin@2
