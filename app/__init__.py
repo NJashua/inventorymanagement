0992858f2cfbfc1beab88aecd830bfa89016afb4
Nithin Burugu <139949160+NJashua@users.noreply.github.com>
2024-05-30 23:24:52 +0530

    improve

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from app.routes import bp
 
-def inventory_management():
+def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp)
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
