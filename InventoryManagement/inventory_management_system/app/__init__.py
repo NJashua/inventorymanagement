f8fe97e6d6281b4c98e2a3c5e718f57744d13b71
NJashua <nithinjashua1@gmail.com>
2024-05-31 23:06:10 +0530

    update code

--- InventoryManagement/inventory_management_system/app/__init__.py
+++ InventoryManagement/inventory_management_system/app/__init__.py
@@ -5,3 +5,7 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp)
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- InventoryManagement/inventory_management_system/config.py
+++ InventoryManagement/inventory_management_system/config.py
@@ -1,12 +1,3 @@
-
-# class Config:
-#     SNOWFLAKE_USER = 'Nithin',
-#     SNOWFLAKE_PASSWORD = 'Nithin@2024',
-#     SNOWFLAKE_ACCOUNT = 'bdhriyc-ke24872',
-#     SNOWFLAKE_DATABASE = 'INVENTORY',
-#     SNOWFLAKE_SCHEMA = 'PUBLIC'
-
-
 # inventory_management_system/config.py
 
 class Config:
@@ -25,3 +16,15 @@ class Config:
             'database': Config.SNOWFLAKE_DATABASE,
             'schema': Config.SNOWFLAKE_SCHEMA
         }
+
+# inventory_management_system/security.py
+
+def get_snowflake_config():
+    config = Config.as_dict()
+    config['password'] = hide_sensitive_data(config['password'])
+    return config
+
+def hide_sensitive_data(data):
+    # Implement a secure way to hide sensitive data,
+    # such as encryption or hashing, before returning the config.
+    return '****'
